72d8bbfc5e7356efbb6bafd0e9b6af115c6d6636
Mathew Glodack <glodack@gmail.com>
2016-08-18 02:24:05 -0700

    Create red card by country method

---
+++ src/RedCard.API/Controllers/StatsController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RedCard.API.Contexts;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RedCard.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class StatsController : Controller
+    {
+        public StatsController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        readonly ApplicationDbContext _dbContext;
+
+        [HttpGet]
+        public IActionResult RedCardCountForCountry()
+        {
+            var redCardCount = _dbContext.Players
+                .GroupBy(player => player.Country)
+                .Select(grouping => new { country = grouping.Key, redCardCount = grouping.Sum(player => player.RedCards) })
+                .ToArray();
+
+            return new ObjectResult(new { redCardCountForCountry = redCardCount });
+        }
+    }
+}
