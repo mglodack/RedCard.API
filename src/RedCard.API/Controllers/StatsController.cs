e466e3d15f8522d4db37575a6665d943d8c6e1e6
Mathew Glodack <glodack@gmail.com>
2016-08-18 02:34:23 -0700

    Add group by yellow cards count

--- src/RedCard.API/Controllers/StatsController.cs
+++ src/RedCard.API/Controllers/StatsController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using RedCard.API.Contexts;
+using RedCard.API.Models;
 
 // For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -19,15 +20,36 @@ namespace RedCard.API.Controllers
 
         readonly ApplicationDbContext _dbContext;
 
+        [Route("redcards")]
         [HttpGet]
         public IActionResult RedCardCountForCountry()
         {
-            var redCardCount = _dbContext.Players
-                .GroupBy(player => player.Country)
-                .Select(grouping => new { country = grouping.Key, redCardCount = grouping.Sum(player => player.RedCards) })
-                .ToArray();
+            Func<IGrouping<string, Player>, object> getRedCardsForCountry =
+                (grouping) => new { country = grouping.Key, redCardCount = grouping.Sum(player => player.RedCards) };
+
+            var redCardCount = _CardsForCountry(getRedCardsForCountry);
 
             return new ObjectResult(new { redCardCountForCountry = redCardCount });
+        }
+
+        [Route("yellowcards")]
+        [HttpGet]
+        public IActionResult YellowCardCountForCountry()
+        {
+            Func<IGrouping<string, Player>, object> getYellowCardsForCountry =
+                (grouping) => new { country = grouping.Key, yellowCardCount = grouping.Sum(player => player.YellowCards) };
+
+            var yellowCardCount = _CardsForCountry(getYellowCardsForCountry);
+
+            return new ObjectResult(new { yellowCardCountForCountry = yellowCardCount });
+        }
+
+        IEnumerable<object> _CardsForCountry(Func<IGrouping<string, Player>, object> getInfo)
+        {
+            return _dbContext.Players
+                .GroupBy(player => player.Country)
+                .Select(getInfo)
+                .ToArray();
         }
     }
 }
